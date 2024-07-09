914cc25c9726936dc74f0d3563632519ba8fa0a3
Anik-Nath <anikdevnath03112000@gmail.com>
2024-07-09 15:43:23 +0600

    Project initailze and installation

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-red-700 font-bold">
+        MechanicalKeyboardShop-Frontend
+      </h1>
+    </>
+  );
+}
+
+export default App;
